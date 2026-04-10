80f8e24d1e425f1b0f6381b4aec36ae019ee5048
Mahika <marathemahika11@gmail.com>
2026-04-10 22:12:58 +0530

    Configure backend for Vercel Serverless deployment

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,10 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
-    await connectDB();
 });
+
+// Force database connection for Vercel Serverless lambda
+connectDB();
+
+// Export the application for Vercel
+export default app;
